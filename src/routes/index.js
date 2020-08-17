93740d4eabebe65172dc02387937387e82e20b88
KewinLizarraga <kewin392@gmail.com>
2020-08-16 23:35:11 -0500

    [UPDATE] ImageRoutes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const { Router } = require('express')
 const router = Router()
 
 const HomeRoutes = require('./home.routes')
+const ImageRoutes = require('./image.routes')
 
-router.use('/', HomeRoutes)
+router.use('/home', HomeRoutes)
+router.use('/images', ImageRoutes)
 
 module.exports = router
