2c4aa2ae9b074c890ffc644e7efa340d01f0c191
KewinLizarraga <kewin392@gmail.com>
2020-08-16 23:28:06 -0500

    [ADD] image controller added

---
+++ src/controllers/image.controller.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //
+  main: (req, res) => {},
+  create: (req, res) => {
+    res.send('imagen enviada...')
+  },
+  like: (req, res) => {},
+  comment: (req, res) => {},
+  remove: (req, res) => {}
+}
