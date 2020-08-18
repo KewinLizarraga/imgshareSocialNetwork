12b41778e0d737e26e069c87cc438179f0c88ca5
KewinLizarraga <kewin392@gmail.com>
2020-08-18 02:55:47 -0500

    [UPDATE]   * change of destination   + validating the extension if it exists   + fileFilter added

--- src/middlewares/storage.midleware.js
+++ src/middlewares/storage.midleware.js
@@ -5,17 +5,27 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../public/upload/temp'))
+    cb(null, path.join(__dirname, '../public/upload'))
   },
   filename: (req, file, cb) => {
     const name = file.fieldname
     const date = moment(Date.now()).format('YYYYMMDD')
-    let ext = file.mimetype
-    ext = ext.split('/').pop()
-    cb(null, `${name}-${date}-${shortid.generate()}.${ext}`)
+    const ext = file.mimetype.split('/').pop()
+    if (['png', 'jpg', 'jpeg', 'gif'].includes(ext)) {
+      cb(null, `${name}-${date}-${shortid.generate()}.${ext}`)
+    }
   }
 })
 
-const upload = multer({ storage })
+const fileFilter = (req, file, cb) => {
+  const ext = file.mimetype.split('/').pop()
+  if (!['png', 'jpg', 'jpeg', 'gif'].includes(ext)) {
+    req.fileValidationError = 'Only images are allowed.'
+    return cb(null, false, new Error('Only images are allowed.'))
+  }
+  cb(null, true)
+}
+
+const upload = multer({ storage, fileFilter })
 
 module.exports = upload
