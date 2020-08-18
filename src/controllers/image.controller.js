397f1c78061dd451f5de5538f265f6340d2746f7
KewinLizarraga <kewin392@gmail.com>
2020-08-18 02:57:57 -0500

    [UPDATE]   + create: saving in the DB   + calling model image

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -1,8 +1,23 @@
+const { Image } = require('../models')
+
 module.exports = {
-  //
-  main: (req, res) => {},
-  create: (req, res) => {
-    res.send('imagen enviada...')
+  main: (req, res) => {
+    res.send('Llamando a la imagen')
+  },
+  create: async (req, res) => {
+    try {
+      const data = {
+        tittle: req.body.tittle,
+        description: req.body.description,
+        filename: req.file.filename,
+        urlImage: req.file.path.split('src')[1]
+      }
+      const image = await Image.create(data)
+      // res.send('imagen enviada...')
+      res.redirect(`/images/${image._id}`)
+    } catch (error) {
+      res.status(500).json({ error: req.fileValidationError })
+    }
   },
   like: (req, res) => {},
   comment: (req, res) => {},
