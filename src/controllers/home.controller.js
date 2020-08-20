bcd9991239e5c681f63a81c975575f2ef3a40266
KewinLizarraga <kewin392@gmail.com>
2020-08-20 11:06:19 -0500

    [UPDATE] moment and viewModel added

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,11 +1,17 @@
+const moment = require('moment')
 const { Image } = require('../models')
+const { sidebar } = require('../helpers')
 
 module.exports = {
   home: async (req, res) => {
     const images = await Image.find().sort({ createdAt: -1 })
+    let viewModel = { images: [] }
+    viewModel.images = images
+    viewModel = await sidebar(viewModel)
     res.render('home', {
       pageName: 'Pagina principal',
-      images
+      viewModel,
+      moment
     })
   }
 }
